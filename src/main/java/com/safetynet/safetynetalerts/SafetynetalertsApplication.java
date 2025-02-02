04a78fa690cc60c95c5e99f6dff52185bd83b2e6
Jérémy Radenne <jrmrdnn@gmail.com>
2025-02-02 16:40:18 +0100

    Add config logs

--- src/main/java/com/safetynet/safetynetalerts/SafetynetalertsApplication.java
+++ src/main/java/com/safetynet/safetynetalerts/SafetynetalertsApplication.java
@@ -3,10 +3,15 @@ package com.safetynet.safetynetalerts;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.logging.Logger;
+
 @SpringBootApplication
 public class SafetynetalertsApplication {
 
+	private static final Logger logger = Logger.getLogger(SafetynetalertsApplication.class.getName());
+
 	public static void main(String[] args) {
+		logger.info("Application started 🚀");
 		SpringApplication.run(SafetynetalertsApplication.class, args);
 	}
 
